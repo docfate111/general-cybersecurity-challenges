a327c9efad2086b9814f6e375b427b5c6324021e
docfate111 <tdwilliamsiv@gmail.com>
2020-08-11 18:48:10 -0500

    general xor

--- crypto/CryptoHack/general/xor.py
+++ crypto/CryptoHack/general/xor.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+from PIL import Image
+import numpy as np
 from pwn import *
 # Given the string "label", XOR each character with the integer 13
 xorn = lambda s, n: ''.join([chr(ord(i)^n) for i in s])
@@ -12,10 +15,39 @@ print(s.decode('utf-8'))
 # I've hidden my data using XOR with a single byte. Don't forget to decode from hex first.
 a = '73626960647f6b206821204f21254f7d694f7624662065622127234f726927756d'
 b = bytes.fromhex(a)
-#
 for i in range(20):
     s = xor(bytes(chr(i), 'utf-8'), b).decode('utf-8')
     if 'crypto' in s:
         print(s)
 #I've encrypted the flag with my secret key, you'll never be able to guess it.
-#'0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104'
+# convert 'crypto{' into binary and xor a(hex string) then xor to find the original key
+# the original key is stored in ans and then xored with the hex string
+# to get the flag
+a = '0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104'
+st='crypto{'
+given = [format(ord(x), 'b') for x in st][:7]
+key = [format(x, 'b') for x in bytes.fromhex(a)][:7]
+def xor_str(x, y):
+    return '{0:b}'.format(int(x, 2) ^ int(y, 2))
+ans = ''
+for i in range(7):
+    ans += chr(int(xor_str(given[i], key[i]), 2))
+ans += 'y'
+print(ans)
+key = [format(x, 'b') for x in bytes.fromhex(a)]
+key2 = [bin(ord(i))[2:] for i in ans*5+ans[:2]]
+resp = ''
+for i in range(len(key2)):
+    resp += chr(int(xor_str(key[i], key2[i]), 2))
+print(resp)
+def xor_images(a, b):
+    im1 = Image.open(a)
+    im2 = Image.open(b)
+    # Make into Numpy arrays
+    im1np = np.array(im1)*255
+    im2np = np.array(im2)*255
+    # XOR with Numpy
+    result = np.bitwise_xor(im1np, im2np).astype(np.uint8)
+    # Convert back to PIL image and save
+    Image.fromarray(result).save('result.png')
+xor_images('lemur.png', 'flag.png')
