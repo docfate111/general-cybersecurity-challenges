8f8ce666b017a500165314a5fd9360b151da5387
docfate111 <tdwilliamsiv@gmail.com>
2020-08-15 00:28:46 -0500

    idk

--- crypto/CryptoHack/blockcipher/ecb.py
+++ crypto/CryptoHack/blockcipher/ecb.py
@@ -2,57 +2,32 @@ import requests
 from string import printable
 from Cryptodome.Cipher import AES
 from Cryptodome.Util.Padding import pad, unpad
-def split_len(seq, length):
-    return [seq[i:i+length] for i in range(0, len(seq), length)]
-
-def oracle(chosen):
-    secret = ("a"*46) + chosen # target to decrypt
-    secret = getPadding(secret)
-    if display:
-        displaySecret(secret) # For illustrative purposes
-    ct = cipher.encrypt(secret)
-    return ct
-
-def getPadding(secret):
-    pl = len(secret)
-    mod = pl % 16
-    if mod != 0:
-        padding = 16 - mod
-        secret += "X" * padding
-    return secret
-
-def displaySecret(secret):
-    split = split_len(secret, 16)
-    display = ""
-    for i in split:
-        for j in split_len(i, 1):
-            display += j + " "
-        display += " "
-    print("pt: %s" % display)
-
-def displayCiphertext(ct):
-    split = split_len(ct, 16)
-    display = ""
-    for i in split:
-        display += i.encode('hex') + " "
-    print("ct: %s" % (display))
-
+def print_in_blocks_of(s, n):
+    for i in range(0, len(s), n):
+        print(s[i:i+n])
 if __name__ == "__main__":
     p = 'a'*46
-    each_letter = {}
+    site = 'http://aes.cryptohack.org/ecb_oracle/encrypt/{plaintext}/'.format(plaintext=p)
+    r = requests.get(site)
+    saved = r.json()['ciphertext']
+    print_in_blocks_of(saved, 32)
+    p = 'a'*44
     for c in printable:
         site = 'http://aes.cryptohack.org/ecb_oracle/encrypt/{plaintext}/'.format(plaintext=p+c.encode('utf-8').hex())
         r = requests.get(site)
-        if(r.status_code==200):
-            resp = r.json()['ciphertext']
-            each_letter[c] = resp
-        else:
-            print(r.text)
-    for i in each_letter.keys():
-        print(each_letter, each_letter[i])
-    chosen = sys.argv[1]
-    ct = oracle(chosen)
-    if display:
-        displayCiphertext(ct)
-    else:
-        print(ct.hex())
+        resp = r.json()['ciphertext']
+        if resp[32:38] == saved[32:38]:
+            print('='*20)
+            print(c)
+            print_in_blocks_of(resp, 32)
+            print('='*20)
+    # by testing the length of input and output we determined that the input
+    # must be an even length and that the blocks are 16 bytes
+    # for i in range(2, 120, 2):
+    #     p = 'a'*i
+    #     site = 'http://aes.cryptohack.org/ecb_oracle/encrypt/{plaintext}/'.format(plaintext=p)
+    #     r = requests.get(site)
+    #     if(r.status_code==200):
+    #         print(len(p), len(r.json()['ciphertext']))
+    #     else:
+    #         print(r.text)
