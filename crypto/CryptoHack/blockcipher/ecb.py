94ed535ee0e6ad1759da255319f79b0cc038f7c4
docfate111 <tdwilliamsiv@gmail.com>
2020-08-15 15:33:55 -0500

    solved

--- crypto/CryptoHack/blockcipher/ecb.py
+++ crypto/CryptoHack/blockcipher/ecb.py
@@ -11,30 +11,36 @@ def print_blocks(n):
     for i in n:
         print(i)
 if __name__ == "__main__":
-    block_size = 16
-    ind = 0
+    # lets get 2 blocks
+    block_size = 32
+    ind = 1
     flag = ''
-    while(ind<15):
-        p = (15-ind)*'a'
+    while(ind<block_size):
+        p = (block_size-ind)*'a'
         print(p)
         site = 'http://aes.cryptohack.org/ecb_oracle/encrypt/{plaintext}/'.format(plaintext=p.encode('utf-8').hex())
         r = requests.get(site)
         saved = r.json()['ciphertext']
         first = split_blocks(saved, block_size)
-        for c in printable[:93]:
-            p = (15-ind)*'a'+flag+c
+        for c in printable:
+            p = (block_size-ind)*'a'+flag+c
             print(p)
             site = 'http://aes.cryptohack.org/ecb_oracle/encrypt/{plaintext}/'.format(plaintext=p.encode('utf-8').hex())
             r = requests.get(site)
-            resp = r.json()['ciphertext']
-            second = split_blocks(resp, block_size)
-            if second[0]==first[0]:
-                print('='*20)
-                print('Found')
-                print('='*20)
-                flag += c
-                ind += 1
-                break
+            if(r.status_code==200):
+                resp = r.json()['ciphertext']
+                second = split_blocks(resp, block_size)
+                if second[1]==first[1]:
+                    print('='*20)
+                    print('Found')
+                    print('='*20)
+                    flag += c
+                    ind += 1
+                    break
+            else:
+                print("="*20)
+                print(r.text)
+                print("="*20)
     # by testing the length of input and output we determined that the input
     # must be an even length and that the blocks are 16 bytes
     # for i in range(2, 120, 2):
